7b450c056816e647ace324b7312fe234df909241
MiyuruWijesinghe <mkwijesinghe2016@gmail.com>
2021-09-29 12:48:55 +0530

    added seller and removed unwanted files

---
+++ src/components/Navbar/SellerSideNav.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export class SellerSideNav extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="sidenav" >
+                    <a href="/seller" ><i className="fa fa-dashboard"></i>&nbsp; Dashboard</a>
+                    <a href="/items-admin"><i className="fa fa-shopping-cart"></i>&nbsp; Item</a>
+                    <a href="/researches-admin"><i className="fa fa-file-text-o"></i>&nbsp; Reports</a>
+                </div>
+            </div>
+        );
+    }
+}
+export default SellerSideNav;
